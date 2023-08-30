f3a5c9532c267e854fea2a73a5f4b231f42f7d51
tsui66 <quinton.xu@gmail.com>
2023-08-31 00:07:42 +0800

    feat: Implement email templates and footer component
    - Added a new file `footer.tsx` in the `emails/components` directory
    - Implemented a functional component `Footer` for rendering email footers with conditional rendering based on the `marketing` prop
    - Created a `.env.example` file with a comment section and an empty line
    - Added a `RESEND_API_KEY` variable in the `.env.example` file
    - Added a new file `feedback-email.tsx` with a default export function `FeedbackEmail` for generating feedback email templates
    - Added a `sendEmail` function in the `emails/index.ts` file to send emails using the `Resend` library and handle various fields and headers of the email

---
+++ emails/feedback-email.tsx
@@ -0,0 +1,38 @@
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Tailwind,
+  Text,
+} from "@react-email/components";
+
+export default function FeedbackEmail({
+  email = "panic@thedis.co",
+  feedback = "I love Coding!",
+}: {
+  email: string;
+  feedback: string;
+}) {
+  return (
+    <Html>
+      <Head />
+      <Preview>New Feedback Received</Preview>
+      <Tailwind>
+        <Body className="mx-auto my-auto bg-white font-sans">
+          <Container className="mx-auto my-10 max-w-[500px] rounded border border-solid border-gray-200 px-10 py-5">
+            <Heading className="mx-0 my-7 p-0 text-center text-xl font-semibold text-black">
+              New Feedback Received
+            </Heading>
+            <Text className="text-sm leading-6 text-black">
+              New feedback from <span className="font-semibold">{email}</span>
+            </Text>
+            <Text className="text-sm leading-6 text-black">{feedback}</Text>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
